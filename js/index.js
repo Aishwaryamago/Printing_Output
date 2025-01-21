c884677309dc1dc0829326f1b04830ef242f0836
Aishwaryamago <mago0006@algonquinlive.com>
2025-01-21 08:54:48 -0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,41 @@
+//TASK 1-----------------
+let a = 3;
+let b = 5;
+let c;
+
+console.log(`
+a + b: ${a + b}
+a - b: ${a - b}
+a * b: ${a * b}
+a / b: ${a / b}
+a % b: ${a % b}
+a += b: ${a}
+a -= b: ${a}
+a *= b: ${a}
+a /= b: ${a}
+a %= b: ${a}
+a == b: ${a == b}
+a != b: ${a != b}
+a > b: ${a > b}
+a < b: ${a < b}
+!a && !c: ${!a && !c}
+!a || !c: ${!a || !c}
+`);
+
+
+
+//TASK 2----------------------
+
+// Declare variable first_name and assign it with value "Aishwarya".
+let first_name = "Aishwarya";
+// Declare variable last_name and assign it with value "Mago".
+let last_name = "Mago";
+
+// Declare variable email and assign it with aishwaryamago7@gmail.com
+let email = "aishwaryamago7@gmail.com";
+
+//create expression using "My name is Your-first-name Your-last-name. You can contact me at your-email@mail.com."; 
+let output = `My name is ${first_name} ${last_name}. You can contact me at ${email}.`;
+
+//printing output in the browser console
+console.log(output);
